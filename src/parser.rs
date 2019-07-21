e728ebbd1fba4a3089b912e1b72bd27915ac34cf
Gabi Purcaru <gabi@purcaru.com>
2019-07-21 13:51:03 +0100

    added support for multiline parsing

---
+++ src/parser.rs
@@ -0,0 +1,27 @@
+use crate::defaults::ENVIRONMENT;
+use crate::types::*;
+
+use lalrpop_util::ParseError;
+
+lalrpop_mod!(language);
+
+pub type ParsingError<'a> = ParseError<usize, language::Token<'a>, &'static str>;
+
+pub fn parse_single<'input>(
+    env: &mut Environment,
+    input: &'input str,
+) -> Result<Value, ParsingError<'input>> {
+    language::TermParser::new().parse(env, input)
+}
+
+pub fn parse<'input>(input: &'input str) -> Vec<Result<Value, ParsingError<'input>>> {
+    // parse the input line by line
+    let lines: Vec<&str> = input.split("\n").collect();
+    let mut env = ENVIRONMENT.clone();
+    let mut res = Vec::new();
+    for line in lines.iter() {
+        res.push(language::TermParser::new().parse(&mut env, line))
+    }
+
+    res
+}
--- src/test_parser.rs
+++ src/test_parser.rs
@@ -1,43 +1,60 @@
 #[cfg(test)]
 mod tests {
-    use super::super::parser::parse;
-    use super::super::types::*;
+    use super::super::defaults::ENVIRONMENT;
+    use super::super::parser::{parse, parse_single, ParsingError};
+    use super::super::types::{Unit, UnitSet, Value};
+
+    fn parse_helper(input: &'static str) -> Result<Value, ParsingError> {
+        parse_single(&mut ENVIRONMENT.clone(), input)
+    }
 
     #[test]
     fn basic_parse() {
-        assert_eq!(parse("$1234"), Ok(Value::simple(1234.0, "$")));
+        assert_eq!(parse_helper("$1234"), Ok(Value::simple(1234.0, "$")));
 
-        assert_eq!(parse("1234 usd"), Ok(Value::simple(1234.0, "usd")),);
+        assert_eq!(parse_helper("1234 usd"), Ok(Value::simple(1234.0, "usd")),);
     }
 
     #[test]
     fn parse_negation() {
-        assert_eq!(parse("-1234   usd"), Ok(Value::simple(-1234.0, "usd")),);
+        assert_eq!(
+            parse_helper("-1234   usd"),
+            Ok(Value::simple(-1234.0, "usd")),
+        );
     }
 
     #[test]
     fn parse_addition() {
-        assert_eq!(parse("123 usd + 12 usd"), Ok(Value::simple(135.0, "usd")),);
-
-        assert_eq!(parse("123 usd - 12 usd"), Ok(Value::simple(111.0, "usd")),);
+        assert_eq!(
+            parse_helper("123 usd + 12 usd"),
+            Ok(Value::simple(135.0, "usd")),
+        );
 
         assert_eq!(
-            parse("-123 usd - 12 usd * 14"),
+            parse_helper("123 usd - 12 usd"),
+            Ok(Value::simple(111.0, "usd")),
+        );
+
+        assert_eq!(
+            parse_helper("-123 usd - 12 usd * 14"),
             Ok(Value::simple(-291.0, "usd")),
         );
 
-        assert_eq!(parse("2 * 3 + 4"), Ok(Value::unitless(10.0)),);
+        assert_eq!(parse_helper("2 * 3 + 4"), Ok(Value::unitless(10.0)),);
 
-        assert_eq!(parse("2 * (3 + 4)"), Ok(Value::unitless(14.0)),);
+        assert_eq!(parse_helper("2 * (3 + 4)"), Ok(Value::unitless(14.0)),);
     }
 
     #[test]
     fn parse_power() {
-        assert_eq!(parse("2 ** 3"), Ok(Value::unitless(8.0)),);
-        assert_eq!(parse("($2) ** 3"), Ok(Value::new(8.0, units!("$" to 3))),);
+        assert_eq!(parse_helper("2 ** 3"), Ok(Value::unitless(8.0)),);
+        assert_eq!(
+            parse_helper("($2) ** 3"),
+            Ok(Value::new(8.0, units!("$" to 3))),
+        );
 
         assert_eq!(
-            parse("($1 + $1) ** (1 + 4 - 2)"),
+            parse_helper("($1 + $1) ** (1 + 4 - 2)"),
             Ok(Value::new(8.0, units!("$" to 3))),
         );
     }
